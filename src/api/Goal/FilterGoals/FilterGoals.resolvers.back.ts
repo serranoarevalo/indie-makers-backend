4781a9acee4802afe490262b88a79818cf716b93
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-01 23:42:34 +0900

    date_trunc

--- src/api/Goal/FilterGoals/FilterGoals.resolvers.back.ts
+++ src/api/Goal/FilterGoals/FilterGoals.resolvers.back.ts
@@ -1,5 +1,4 @@
 import { getConnection } from "typeorm";
-import Goal from "../../../entities/Goal";
 import User from "../../../entities/User";
 import {
   FilterGoalsQueryArgs,
@@ -17,26 +16,27 @@ const resolvers: Resolvers = {
       const defaultPage = page || 0;
       let makers: User[];
       let totalPages = 0;
-      // We can look for goals and group them by day.
-      // Then we group them by creator
-      // Then we call and join the creator
-      // SELECT date_trunc('day', "updatedAt") FROM goal GROUP BY 1
       try {
         switch (status) {
           case "PENDING":
-            const goals = await getConnection()
-              .getRepository(Goal)
-              .query(
-                `SELECT date_trunc('day', "updatedAt") as date FROM goal GROUP BY 1`
-              );
-            console.log(goals);
-            return {
-              ok: true,
-              error: null,
-              makers: null,
-              page: page || 0,
-              totalPages: Math.floor(totalPages / 25)
-            };
+            [makers, totalPages] = await getConnection()
+              .getRepository(User)
+              .createQueryBuilder("user")
+              .innerJoinAndSelect(
+                "user.goals",
+                "goals",
+                "goals.isCompleted = :isCompleted AND goals.createdAt >= NOW() - :time::INTERVAL AND goals.createdAt <= NOW() - :lessDay::INTERVAL",
+                {
+                  isCompleted: false,
+                  time: `${defaultPage + 1} day`,
+                  lessDay: `${defaultPage} day`
+                }
+              )
+              .innerJoinAndSelect("goals.product", "product")
+              .take(take || 25)
+              .skip(25 * defaultPage)
+              .orderBy("user.updatedAt", "DESC")
+              .getManyAndCount();
             break;
           case "COMPLETED":
             [makers, totalPages] = await getConnection()
@@ -87,7 +87,6 @@ const resolvers: Resolvers = {
           totalPages: Math.floor(totalPages / 25)
         };
       } catch (error) {
-        console.log(error);
         return {
           ok: false,
           error,
